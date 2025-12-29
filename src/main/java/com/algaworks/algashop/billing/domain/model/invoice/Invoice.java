96ab6247e01625dfe1e83aab0446663d80a20f55
Antonio Pires <tonan@outlook.com.br>
2025-12-29 07:51:01 -0300

    Adding persistence to Entities

--- src/main/java/com/algaworks/algashop/billing/domain/model/invoice/Invoice.java
+++ src/main/java/com/algaworks/algashop/billing/domain/model/invoice/Invoice.java
@@ -2,6 +2,7 @@ package com.algaworks.algashop.billing.domain.model.invoice;
 
 import com.algaworks.algashop.billing.domain.model.DomainException;
 import com.algaworks.algashop.billing.domain.model.IdGenerator;
+import jakarta.persistence.*;
 import lombok.*;
 import org.apache.commons.lang3.StringUtils;
 
@@ -14,8 +15,10 @@ import java.util.*;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
 public class Invoice {
 
+    @Id
     @EqualsAndHashCode.Include
     private UUID id;
 
@@ -29,8 +32,10 @@ public class Invoice {
 
     private BigDecimal totalAmount;
 
+    @Enumerated(EnumType.STRING)
     private InvoiceStatus status;
 
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
     private PaymentSettings paymentSettings;
 
     private Set<LineItem> items = new HashSet<>();
@@ -128,6 +133,7 @@ public class Invoice {
                     this.getId(), this.getStatus().toString().toLowerCase()));
         }
         PaymentSettings ps = PaymentSettings.brandNew(method, creditCardId);
+        ps.setInvoice(this);
         this.setPaymentSettings(ps);
     }
 }
